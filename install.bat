f032f23eaa6be5986b6883f6bd8a0a7bec80855f
chris <rsirhc@gmail.com>
2010-06-07 14:48:31 +1000

    added java and fixed install.bat

--- install.bat
+++ install.bat
@@ -7,12 +7,19 @@ echo Copying .emacs
 xcopy /q /y /v ".emacs" "%APPDATA%\.emacs"
 echo Copying .emacs.d/*
 xcopy /q /y /v ".emacs.d\*.el" "%APPDATA%\.emacs.d\"
+if not exist "%APPDATA%\.emacs.d\elpa" mkdir "%APPDATA%\.emacs.d\elpa"
+xcopy /q /y /v ".emacs.d\elpa\*.el" "%APPDATA%\.emacs.d\elpa\"
 echo Compiling...
 %EMACS%\emacs.exe -batch -f batch-byte-compile "%APPDATA%\.emacs.d\custom.el"
 %EMACS%\emacs.exe -batch -f batch-byte-compile "%APPDATA%\.emacs.d\init.el"
+%EMACS%\emacs.exe -batch -f batch-byte-compile "%APPDATA%\.emacs.d\init-bindings.el"
 %EMACS%\emacs.exe -batch -f batch-byte-compile "%APPDATA%\.emacs.d\init-display.el"
 %EMACS%\emacs.exe -batch -f batch-byte-compile "%APPDATA%\.emacs.d\init-cedet.el"
+%EMACS%\emacs.exe -batch -f batch-byte-compile "%APPDATA%\.emacs.d\init-elpa.el"
 %EMACS%\emacs.exe -batch -f batch-byte-compile "%APPDATA%\.emacs.d\init-cc.el"
+%EMACS%\emacs.exe -batch -f batch-byte-compile "%APPDATA%\.emacs.d\init-ruby.el"
+%EMACS%\emacs.exe -batch -f batch-byte-compile "%APPDATA%\.emacs.d\init-java.el"
+%EMACS%\emacs.exe -batch -f batch-byte-compile "%APPDATA%\.emacs.d\elpa\package.el"
 set EMACS=
 echo.
 echo Done.
