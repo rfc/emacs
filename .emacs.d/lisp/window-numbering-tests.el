9c59d057ba1135119e1f3149897d23d288f6c037
chris <rsirhc@gmail.com>
2012-04-19 16:25:47 +1000

    updated

---
+++ .emacs.d/lisp/window-numbering-tests.el
@@ -0,0 +1,20 @@
+<!DOCTYPE
+ html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+ "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" >
+<head>
+	<title>404 - Punk'd</title>
+	<meta name="author"
+		content="Nikolaj Schumacher"/>
+</head>
+<body>
+
+<h1>404</h1>
+
+<p>Dude! You totally thought there'd be a file here!</p>
+
+
+<p><a href="http://validator.w3.org/check?uri=http://www.nschum.de/404.xhtml">valid <abbr>xhtml</abbr></a></p>
+</body>
+</html>
--- install.bat
+++ install.bat
@@ -15,17 +15,18 @@ xcopy /q /y /v /s ".emacs.d\lisp\*" "%APPDATA%\.emacs.d\lisp\"
 echo Compiling main scripts...
 %EMACS%\emacs.exe -batch -f batch-byte-compile "%APPDATA%\.emacs.d\custom.el"
 %EMACS%\emacs.exe -batch -f batch-byte-compile "%APPDATA%\.emacs.d\init.el"
-%EMACS%\emacs.exe -batch -f batch-byte-compile "%APPDATA%\.emacs.d\init-bindings.el"
-%EMACS%\emacs.exe -batch -f batch-byte-compile "%APPDATA%\.emacs.d\init-display.el"
-%EMACS%\emacs.exe -batch -f batch-byte-compile "%APPDATA%\.emacs.d\init-cedet.el"
-%EMACS%\emacs.exe -batch -f batch-byte-compile "%APPDATA%\.emacs.d\init-elpa.el"
 %EMACS%\emacs.exe -batch -f batch-byte-compile "%APPDATA%\.emacs.d\init-cc.el"
 %EMACS%\emacs.exe -batch -f batch-byte-compile "%APPDATA%\.emacs.d\init-ruby.el"
 %EMACS%\emacs.exe -batch -f batch-byte-compile "%APPDATA%\.emacs.d\init-python.el"
 
-echo Compile CEDET:
-echo - %EMACS%\emacs.exe -Q -l "%APPDATA%\.emacs.d\lisp\cedet\cedet-build.el" -f cedet-build
+echo Compiling 3rd party scripts...
+%EMACS%\emacs.exe -batch -f batch-byte-compile "%APPDATA%\.emacs.d\lisp\window-numbering.el"
 
+echo.
+echo POSTINSTALL:
+echo ------------
+echo 1. Build CEDET: %EMACS%\emacs.exe -Q -l "%APPDATA%\.emacs.d\lisp\cedet\cedet-build.el" -f cedet-build
+echo.
 
 set EMACS=
 echo.
