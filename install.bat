5c719922f273a278cebe77dc6031a43e65bb64d4
chris <rsirhc@gmail.com>
2013-06-24 16:15:12 +1000

    Update install location for Win7. Enable C++

--- install.bat
+++ install.bat
@@ -6,29 +6,29 @@ echo  Installing .emacs and .emacs.d
 echo ================================
 
 echo Copying .emacs
-xcopy /q /y /v ".emacs" "%APPDATA%\.emacs"
+xcopy /q /y /v ".emacs" "%HOME%\.emacs"
 echo Copying .emacs.d/*
-xcopy /q /y /v ".emacs.d\*.el" "%APPDATA%\.emacs.d\"
+xcopy /q /y /v ".emacs.d\*.el" "%HOME%\.emacs.d\"
 echo Copying .emacs.d/lisp/*
-xcopy /q /y /v /s ".emacs.d\lisp\*" "%APPDATA%\.emacs.d\lisp\"
+xcopy /q /y /v /s ".emacs.d\lisp\*" "%HOME%\.emacs.d\lisp\"
 echo Creating .emacs.d/elpa/*
-if not exist "%APPDATA%\.emacs.d\elpa\" mkdir "%APPDATA%\.emacs.d\elpa\"
+if not exist "%HOME%\.emacs.d\elpa\" mkdir "%HOME%\.emacs.d\elpa\"
 echo Creating .emacs.d/snippets/*
-if not exist "%APPDATA%\.emacs.d\snippets\" mkdir "%APPDATA%\.emacs.d\snippets\"
+if not exist "%HOME%\.emacs.d\snippets\" mkdir "%HOME%\.emacs.d\snippets\"
 
 echo Compiling main scripts...
-%EMACS%\emacs.exe -batch -f batch-byte-compile "%APPDATA%\.emacs.d\custom.el"
-%EMACS%\emacs.exe -batch -f batch-byte-compile "%APPDATA%\.emacs.d\init.el"
-%EMACS%\emacs.exe -batch -f batch-byte-compile "%APPDATA%\.emacs.d\init-cc.el"
-%EMACS%\emacs.exe -batch -f batch-byte-compile "%APPDATA%\.emacs.d\init-ruby.el"
-%EMACS%\emacs.exe -batch -f batch-byte-compile "%APPDATA%\.emacs.d\init-python.el"
+%EMACS%\emacs.exe -batch -f batch-byte-compile "%HOME%\.emacs.d\custom.el"
+%EMACS%\emacs.exe -batch -f batch-byte-compile "%HOME%\.emacs.d\init.el"
+%EMACS%\emacs.exe -batch -f batch-byte-compile "%HOME%\.emacs.d\init-cc.el"
+%EMACS%\emacs.exe -batch -f batch-byte-compile "%HOME%\.emacs.d\init-ruby.el"
+%EMACS%\emacs.exe -batch -f batch-byte-compile "%HOME%\.emacs.d\init-python.el"
 
 :: Commented out scripts with warnings
-::%EMACS%\emacs.exe -batch -f batch-byte-compile "%APPDATA%\.emacs.d\init-elpa.el"
+::%EMACS%\emacs.exe -batch -f batch-byte-compile "%HOME%\.emacs.d\init-elpa.el"
 
 ::echo Compiling 3rd party scripts...
-::%EMACS%\emacs.exe -batch -f batch-byte-compile "%APPDATA%\.emacs.d\lisp\window-numbering.el"
-::%EMACS%\emacs.exe -batch -f batch-byte-compile "%APPDATA%\.emacs.d\lisp\package.el"
+::%EMACS%\emacs.exe -batch -f batch-byte-compile "%HOME%\.emacs.d\lisp\window-numbering.el"
+::%EMACS%\emacs.exe -batch -f batch-byte-compile "%HOME%\.emacs.d\lisp\package.el"
 
 echo.
 echo POSTINSTALL:
